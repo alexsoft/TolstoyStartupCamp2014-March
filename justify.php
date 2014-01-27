82812183b4e0a5d3e09d88a2d2f3a2ad71aa2df2
Alexey Plekhanov <alexey.plekhanov@dataart.com>
2014-01-27 18:05:49 +0200

    make a class instead of procedures

--- justify.php
+++ justify.php
@@ -1,25 +1,75 @@
 <?php
 ini_set('display_errors', 1);
+class Justify {
+	const N = 80;
+
+	protected $_paragraphEndSymbols = array('\.', '!', '?');
+	/**
+	 * @var string
+	 */
+	protected $_inputFileName;
+	/**
+	 * @var string
+	 */
+	protected $_outputFileName;
+
+	protected $_input;
+	protected $_output;
+
+	/**
+	 * Open input and output files
+	 * @param string $inputFileName
+	 * @param string $outputFileName
+	 */
+	public function __construct($inputFileName, $outputFileName) {
+		$this->_inputFileName = $inputFileName;
+		$this->_outputFileName = $outputFileName;
+	}
+
+	public function justify() {
+		if (file_exists($this->_inputFileName)) {
+			$file = fopen($this->_inputFileName, 'r');
+			$this->_input = fread($file, filesize($this->_inputFileName));
+			fclose($file);
+			echo '/([\w\W]*)\.\n/';
+			preg_match_all('/([\w\W]*)\.\n/', $this->_input, $matches);
+			var_dump($matches);
+//			var_dump(explode('.'.PHP_EOL, $this->_input));
+		} else {
+			die("Could not open input file: {$this->_inputFileName}\n");
+		}
+	}
+
+	protected function _parse() {
+
+	}
+
+	/**
+	 * @param string $inputFileName
+	 * @param string $outputFileName
+	 */
+	protected function _openFiles($inputFileName, $outputFileName) {
+		if (file_exists($inputFileName)) {
+			$this->_inputFile = fopen($inputFileName, 'r');
+			echo "Opened input file: {$inputFileName}\n";
+			$this->_outputFile = fopen($outputFileName, 'w');
+		} else {
+			die("Could not open input file: {$inputFileName}\n");
+		}
+	}
+}
 
 if (!isset($argv[1])) {
 	die("Specify input and output files names\n");
 } elseif (!isset($argv[2])) {
 	die("Specify output file name\n");
 } else {
-	define('N', 80);
+	$j = new Justify($argv[1], $argv[2]);
+	$j->justify();
 
-	$inputFileName = $argv[1];
-	$outputFileName = $argv[2];
 
-	if (file_exists($inputFileName)) {
-
-		$inputFile = fopen($inputFileName, 'r');
-		echo "Opened input file: {$inputFileName}\n";
-		$outputFile = fopen($outputFileName, 'w');
-		echo "Opened output file: {$outputFileName}\n";
-
-		$content = fread($inputFile, filesize($inputFileName));
-		var_dump(strpos($content, PHP_EOL));
+//		$content = fread($inputFile, filesize($inputFileName));
+//		var_dump(strpos($content, PHP_EOL));
 		/*
 		$nExploded = array_map(
 			function($s) {
@@ -30,7 +80,7 @@ if (!isset($argv[1])) {
 		*/
 		// var_dump(explode('.' . PHP_EOL, $content));
 		// exit;
-		var_dump(explode('|', str_replace(".".PHP_EOL, '|', $content)));
+//		var_dump(explode('|', str_replace(".".PHP_EOL, '|', $content)));
 		/*
 		foreach ($nExploded as $str) {
 			if (strlen($str) <= (N / 2)) {
@@ -42,11 +92,6 @@ if (!isset($argv[1])) {
 		*/
 
 
-
-
-		fclose($outputFile);
-		fclose($inputFile);
-	} else {
-		die("Could not open input file: {$inputFileName}\n");
-	}
+//		fclose($outputFile);
+//		fclose($inputFile);
 }
