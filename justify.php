9237aaecf0bc0e71eac8f704907c01a9de85c3b2
Alexey Plekhanov <oleksii.plekhanov@gmail.com>
2014-02-01 20:05:28 +0200

    Рефакторинг сделан!

--- justify.php
+++ justify.php
@@ -1,24 +1,61 @@
 <?php
-ini_set('display_errors', 1);
+
+/**
+ * Для запуска требуется PHP >= 5.3.0
+ * (php5-cli для запуска из командной строки)
+ * 
+ * Запускается следующей командой
+ * php justify.php input output
+ * где input - имя файла с входными данными,
+ * output - имя файла, куда будут записаны выходные данные.
+ * 
+ * Если файла не существует, он будет создан.
+ * При повторном запуске output файл будет перезаписан.
+ */
 
 /**
  * Class Justify
  */
 class Justify {
+	/**
+	 * Length of lines
+	 */
 	const N = 80;
 
-	protected $_paragraphEndSymbols = array('.', '!', '?');
-	protected $_indent = '    ';
 	/**
+	 * Punctuation of paragraph end
+	 * @var array
+	 */
+	protected $_paragraphEndSymbols = array('.', '!', '?');
+
+	/**
+	 * Indent for paragraphs
+	 * @var string
+	 */
+	protected $_indent = '    ';
+
+	/**
+	 * Input file name
 	 * @var string
 	 */
 	protected $_inputFileName;
+
 	/**
+	 * Output file name
 	 * @var string
 	 */
 	protected $_outputFileName;
 
+	/**
+	 * Input text
+	 * @var string
+	 */
 	protected $_input;
+
+	/**
+	 * Array of paragraphs for output file
+	 * @var array
+	 */
 	protected $_output = array();
 
 	/**
@@ -31,40 +68,56 @@ class Justify {
 		$this->_outputFileName = $outputFileName;
 	}
 
+	/**
+	 * Justifies input and writes to output file
+	 * @return bool Was writing to file successfull
+	 */
 	public function justify() {
 		if (file_exists($this->_inputFileName)) {
 			$file = fopen($this->_inputFileName, 'r');
 			$this->_input = fread($file, filesize($this->_inputFileName));
 			fclose($file);
 
-			$ar = explode(PHP_EOL, $this->_input);
+			$strings = explode(PHP_EOL, $this->_input);
 			$isInParagraph = FALSE;
 			$tmp = '';
-			foreach ($ar as $str) {
+			foreach ($strings as $str) {
+				// If length of line is less than N/2 and we are not processing
+				// paragraph then it is heading
 				if (mb_strlen($str) < static::N / 2 && !$isInParagraph) {
 					$this->_output[] = $this->_indent . $str;
 				} else {
 					$isInParagraph = TRUE;
 					$tmp .= $str;
+					// If we get to the end of line and any of paragraph ending
+					// punctuation then we need to add this paragraph to array
+					// of paragraphs and start a new one
 					if (in_array($str[mb_strlen($str) - 1], $this->_paragraphEndSymbols)) {
-
-						$str = $this->_processParagraph($tmp);
-						$this->_output[] = $str;
-
+						$this->_output[] = $this->_processParagraph($tmp);
 						$tmp = '';
 						$isInParagraph = FALSE;
 					}
 				}
 			}
-			$o = fopen($this->_outputFileName, 'w');
-			fwrite($o, implode(PHP_EOL, $this->_output));
-			fclose($o);
-			// var_dump($this->_output);
+
+			// Write to output file
+			$file = fopen($this->_outputFileName, 'w');
+			$isSuccessful = fwrite($file, implode(PHP_EOL, $this->_output));
+			fclose($file);
+
+			return $isSuccessful;
 		} else {
 			die("Could not open input file: {$this->_inputFileName}\n");
 		}
 	}
 
+	/**
+	 * Adds indent for paragraph then
+	 * splits paragraph into lines then
+	 * adds spaces for justifying
+	 * @param  string $str Paragraph to be processed
+	 * @return string      Processed paragraph
+	 */
 	protected function _processParagraph($str) {
 		$str = $this->_indent . $str;
 		$iteration = 0;
@@ -87,31 +140,30 @@ class Justify {
 		return $str;
 	}
 
+	/**
+	 * Adds spaces for justifying
+	 * @param  string $str Paragraph to which we need to add spaces
+	 * @return string      Justified paragraph
+	 */
 	protected function _justifyParagraph($str) {
 		$rows = explode(PHP_EOL, $str);
-		$k = 0;
+		$iteration = 0;
 		foreach ($rows as &$row) {
-			if ($k != count($rows) - 1) {
-				$l = mb_strlen($row) - 1;
+			if ($iteration != count($rows) - 1) {
+				$end = ($iteration == 0) ? mb_strlen($this->_indent) : 0;
 
-				$t = ($k == 0) ? mb_strlen($this->_indent) : 0;
-
-				for ($i = $l; $i >= $t; $i--) {
+				for ($i = mb_strlen($row) - 1; $i >= $end; $i--) {
 					if (mb_strlen($row) >= static::N) {
 						break;
 					} else {
 						if ($row[$i] === ' ') {
-							$tmp = mb_substr($row, 0, $i) . '  ' . mb_substr($row, $i+1);
-							// var_dump($tmp);
-							$row = $tmp;
-							var_dump($row);
+							$row = mb_substr($row, 0, $i) . '  ' . mb_substr($row, $i+1);
 						}
 					}
 				}
 			}
-			$k++;
+			$iteration++;
 		}
-		var_dump($rows);
 		return implode(PHP_EOL, $rows);
 	}
 }
@@ -122,5 +174,7 @@ if (!isset($argv[1])) {
 	die("Specify output file name\n");
 } else {
 	$j = new Justify($argv[1], $argv[2]);
-	$j->justify();
+	if ($j->justify()) {
+		echo "Text was successfully justified!\n";
+	};
 }
